414b896b0dd9638dfb2de017074a4acb44b113b1
caisarl76 <caisarl76@gmail.com>
2021-07-14 20:59:47 +0900

    add lt dataset

--- train_2stage.py
+++ train_2stage.py
@@ -91,7 +91,7 @@ def main():
     args = parser.parse_args()
     expname = '_'.join(
         [args.dataset, args.imb_type, (str)(args.imb_factor), args.net_type, (str)(args.depth),
-         args.sample_method, (str)(args.beta), (str)(args.cutmix_prob), args.loss_type, ('lr' + (str)(args.lr))])
+         args.sample_method, (str)(args.beta), (str)(args.cutmix_prob), args.loss_type, ('lr' + (str)(args.lr)), args.sampler])
 
     args.expname = os.path.join('runs', 'two_stage', expname)
     if not os.path.exists(args.expname):
